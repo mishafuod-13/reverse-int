35fa908df9dc04b50696dece115565b4b02af1d9
mishafuod-13 <mishafuod@gmail.com>
2021-03-12 03:46:14 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+    let str = n+"";
+    let sol = "";
+
+  for (let i = str.length-1; i>=0; i--){
+    if (str[i]==="-"){
+      continue;
+    } else if (str[i]==="0"&&sol.length==0){
+      continue;
+    } else {
+      sol+=str[i];
+    }
+  }
+
+  return parseInt(sol, 10);
 }
